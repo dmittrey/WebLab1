f7f6aaf8aa6d497738954e7defbf5f8775e49d56
dmittrey <dmitryzubakhin@mail.ru>
2021-09-03 21:30:50 +0300

    table done

--- src/js/table_ajax.js
+++ src/js/table_ajax.js
@@ -2,16 +2,18 @@ function receiveSubmit() {
 
     if (validateForm(true)) {
 
-        let msg = $('#formx').serialize();
+        let Y_value = $('#Y_value').val();
+        let X_value = $('#X_value').val();
+        let R_value = $('#R_value').val();
 
         $.ajax({
             type: 'POST',
             url: '../src/php/upload.php',
-            data: msg,
+            data: {'y':Y_value, 'x':X_value, 'r':R_value},
             success: function (data) {
-                $('table').html(data);
+                $('#table tr:last').after(data);
             },
-            error: function (xhr, str) {
+            error: function (xls, str) {
                 alert("Возникла ошибка: " + str);
             }
         })
--- src/php/upload.php
+++ src/php/upload.php
@@ -1,27 +1,29 @@
 <?php
 
-if (isset($_POST["Y_value"]) && isset($_POST["X_value"]) && isset($_POST["R_value"])) {
-    if (validate($_POST["Y_value"], $_POST["X_value"], $_POST["R_value"])) {
+require_once "hit_result.php";
+require_once "validator.php";
 
-        $x = $_POST["X_value"];
-        $y = $_POST["Y_value"];
-        $r = $_POST["R_value"];
+date_default_timezone_set('Europe/Moscow');
 
-        $hit_result = hit_result($y, $x, $r);
+$start = microtime(true);
+$current_time = date("H:i:s");
 
-        echo $data = <<<_END
+$x = $_POST["x"];
+$y = $_POST["y"];
+$r = $_POST["r"];
+
+$hit_result = hit_result($y, $x, $r) ? "TRUE" : "FALSE";
+
+$script_time = number_format(microtime(true) - $start, 8, ".", "") * 1000000;
+
+echo <<<_END
         <tr>
             <th>$x</th>
-            <th$y</th>
-            <th$r</th>
-            <th>time</th>
-            <th>php_time</th>
+            <th>$y</th>
+            <th>$r</th>
+            <th>$current_time</th>
+            <th>$script_time</th>
             <th>$hit_result</th>
         </tr>
 _END;
-        return;
-    }
-
-    echo 'Валидация не прошла!';
-    return;
-}
+return;
